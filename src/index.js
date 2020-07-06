2dd51366b0b33645150b04d43181fbc1747abc98
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2020-07-06 22:42:12 +0300

    task 1: add version and description output

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import program from 'commander';
+
 export default () => {
-  console.log('Hello, World!');
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.');
+
+  program.parse(process.argv);
 };
