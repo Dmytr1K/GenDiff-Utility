8efc3df35f11aadb4433fc139ba9febc7a32d775
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2020-07-16 19:51:21 +0300

    task 5: implement YAML-files compare

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 import getDiff from './getDiff.js';
 
+// eslint-disable-next-line consistent-return
+const parse = (filepath) => {
+  const format = path.extname(filepath);
+  const content = fs.readFileSync(filepath, 'utf-8');
+
+  if (format === '.json') {
+    return JSON.parse(content);
+  }
+  if (format === '.yml') {
+    return yaml.safeLoad(content);
+  }
+};
+
 const genDiff = (filepath1, filepath2) => {
-  const config1 = JSON.parse(fs.readFileSync(filepath1));
-  const config2 = JSON.parse(fs.readFileSync(filepath2));
+  const config1 = parse(filepath1);
+  const config2 = parse(filepath2);
 
   return getDiff(config1, config2);
 };
