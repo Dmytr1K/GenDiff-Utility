fb95a6084a3930ae9eca93d986eed9e71d6f2ac5
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2020-07-24 20:51:10 +0300

    task 7: implement nested files compare

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,37 +1,21 @@
 import _ from 'lodash';
 
-const getDiff = (config1, config2) => {
-  const keys1 = Object.keys(config1);
-  const keys2 = Object.keys(config2);
+const buildDiff = (contentBefore, contentAfter) => {
+  const keys = _.union(Object.keys(contentBefore), Object.keys(contentAfter));
 
-  const unionKeys = _.union(keys1, keys2);
-  const intersectionKeys = _.intersection(keys1, keys2);
-  const unchangedKeys = intersectionKeys
-    .filter((key) => config1[key] === config2[key]);
-  const changedKeys = _.difference(intersectionKeys, unchangedKeys);
-  const newKeys = _.difference(keys2, keys1);
-  const removedKeys = _.difference(keys1, keys2);
-
-  const addData = (acc, key) => {
-    if (unchangedKeys.includes(key)) {
-      acc.push(`\n    ${key}: ${config2[key]}`);
+  const addEntry = (key) => {
+    const valueBefore = contentBefore[key];
+    const valueAfter = contentAfter[key];
+    const entry = { name: key };
+    if (_.isObject(valueBefore) && _.isObject(valueAfter)) {
+      entry.objectsDifference = buildDiff(valueBefore, valueAfter);
+    } else {
+      entry.valuesPair = [valueBefore, valueAfter];
     }
-    if (removedKeys.includes(key)) {
-      acc.push(`\n  - ${key}: ${config1[key]}`);
-    }
-    if (newKeys.includes(key)) {
-      acc.push(`\n  + ${key}: ${config2[key]}`);
-    }
-    if (changedKeys.includes(key)) {
-      acc.push(`\n  + ${key}: ${config2[key]}\n  - ${key}: ${config1[key]}`);
-    }
-    return acc;
+    return entry;
   };
 
-  const result = unionKeys.reduce(addData, []);
-  const formatResult = `{${result.join('')}\n}`;
-
-  return formatResult;
+  return keys.map(addEntry);
 };
 
-export default getDiff;
+export default buildDiff;
