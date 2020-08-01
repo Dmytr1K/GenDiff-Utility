32d10540296ec9a847b8e424d26c389b034b6079
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2020-08-01 13:19:16 +0300

    final fixes: move the computation the type of difference to buildDiff.js

--- src/utils.js
+++ src/utils.js
@@ -6,17 +6,3 @@ export const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 export const getFileExtension = (filePath) => path.extname(filePath).substring(1);
 
 export const convert = (data, type, converters) => converters[type](data);
-
-export const compareValues = (valuesPair) => {
-  const [valueBefore, valueAfter] = valuesPair;
-  if (valueBefore === undefined) {
-    return 'added';
-  }
-  if (valueAfter === undefined) {
-    return 'removed';
-  }
-  if (valueBefore !== valueAfter) {
-    return 'updated';
-  }
-  return 'unchanged';
-};
