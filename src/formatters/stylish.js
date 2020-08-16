85586ee6688d27a331937932ea414a32510fe9c1
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2020-08-16 15:22:00 +0300

    final fixes: refactor - move render call to stringify function

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -16,41 +16,33 @@ const prefixChars = {
 
 const getIndent = (depth) => filler.repeat(indentSize * depth);
 const getPrefix = (type) => filler.repeat(prefixIndentSize) + prefixChars[type] + filler;
-const stringify = (name, value, depth, type) => getIndent(depth) + getPrefix(type)
-  + name + separator + filler + value;
-
-const render = (data, depth) => {
+const render = (data, depth, stringify) => {
   if (!_.isObject(data)) return data;
   const newDepth = depth + 1;
   const entries = Object.entries(data);
-  const strings = entries.flatMap(([key, value]) => [stringify(key, render(value, newDepth), newDepth, 'blank')]);
+  const strings = entries
+    .flatMap(([key, value]) => [stringify(key, render(value, newDepth, stringify), newDepth, 'blank')]);
   const framedStrigs = [frameChars.initial, ...strings, getIndent(newDepth) + frameChars.final];
   return framedStrigs.join('\n');
 };
+const stringify = (name, value, depth, type) => getIndent(depth) + getPrefix(type)
+  + name + separator + filler + render(value, depth, stringify);
 
 const format = (diffTree) => {
   const builders = {
-    unchanged: (node, depth) => {
-      const value = render(node.value, depth);
-      return stringify(node.name, value, depth, 'blank');
-    },
-    removed: (node, depth) => {
-      const value = render(node.value, depth);
-      return stringify(node.name, value, depth, 'minus');
-    },
-    added: (node, depth) => {
-      const value = render(node.value, depth);
-      return stringify(node.name, value, depth, 'plus');
-    },
-    changed: (node, depth) => {
-      const valueBefore = render(node.valueBefore, depth);
-      const valueAfter = render(node.valueAfter, depth);
-      return [
-        stringify(node.name, valueBefore, depth, 'minus'),
-        stringify(node.name, valueAfter, depth, 'plus'),
-      ];
-    },
-    nested: (node, depth, innerFormat) => stringify(node.name, innerFormat(node.children, depth + 1), depth, 'blank'),
+    unchanged: (node, depth) => stringify(node.name, node.value, depth, 'blank'),
+    removed: (node, depth) => stringify(node.name, node.value, depth, 'minus'),
+    added: (node, depth) => stringify(node.name, node.value, depth, 'plus'),
+    changed: (node, depth) => [
+      stringify(node.name, node.valueBefore, depth, 'minus'),
+      stringify(node.name, node.valueAfter, depth, 'plus'),
+    ],
+    nested: (node, depth, innerFormat) => stringify(
+      node.name,
+      innerFormat(node.children, depth + 1),
+      depth,
+      'blank',
+    ),
   };
 
   const innerFormat = (innerDiffTree, depth) => {
