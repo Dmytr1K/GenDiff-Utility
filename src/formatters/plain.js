495bdc4fe49276ebf63e71c29b586044a63137fc
Dmytro Korzhov <dmytro.korzhov@gmail.com>
2020-08-19 19:42:32 +0300

    final fixes: refactor - make small fixes

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,20 +4,19 @@ const stringify = (value) => {
     string: (data) => `'${data}'`,
     default: (data) => data,
   };
-
   const convert = converters[typeof value] || converters.default;
+
   return convert(value);
+};
+const mapping = {
+  unchanged: () => [],
+  removed: (property) => `Property '${property}' was removed`,
+  added: (property, node) => `Property '${property}' was added with value: ${stringify(node.value)}`,
+  changed: (property, node) => `Property '${property}' was updated. From ${stringify(node.valueBefore)} to ${stringify(node.valueAfter)}`,
+  nested: (property, node, innerFormat) => innerFormat(node.children, property),
 };
 
 const format = (diffTree) => {
-  const mapping = {
-    unchanged: () => [],
-    removed: (property) => `Property '${property}' was removed`,
-    added: (property, node) => `Property '${property}' was added with value: ${stringify(node.value)}`,
-    changed: (property, node) => `Property '${property}' was updated. From ${stringify(node.valueBefore)} to ${stringify(node.valueAfter)}`,
-    nested: (property, node, innerFormat) => innerFormat(node.children, property),
-  };
-
   const innerFormat = (diff, path) => {
     const callback = (node) => {
       const { name, type } = node;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -27,24 +27,23 @@ const render = (data, depth, stringify) => {
 };
 const stringify = (name, value, depth, type) => getIndent(depth) + getPrefix(type)
   + name + separator + filler + render(value, depth, stringify);
+const mapping = {
+  unchanged: (node, depth) => stringify(node.name, node.value, depth, 'blank'),
+  removed: (node, depth) => stringify(node.name, node.value, depth, 'minus'),
+  added: (node, depth) => stringify(node.name, node.value, depth, 'plus'),
+  changed: (node, depth) => [
+    stringify(node.name, node.valueBefore, depth, 'minus'),
+    stringify(node.name, node.valueAfter, depth, 'plus'),
+  ],
+  nested: (node, depth, innerFormat) => stringify(
+    node.name,
+    innerFormat(node.children, depth + 1),
+    depth,
+    'blank',
+  ),
+};
 
 const format = (diffTree) => {
-  const mapping = {
-    unchanged: (node, depth) => stringify(node.name, node.value, depth, 'blank'),
-    removed: (node, depth) => stringify(node.name, node.value, depth, 'minus'),
-    added: (node, depth) => stringify(node.name, node.value, depth, 'plus'),
-    changed: (node, depth) => [
-      stringify(node.name, node.valueBefore, depth, 'minus'),
-      stringify(node.name, node.valueAfter, depth, 'plus'),
-    ],
-    nested: (node, depth, innerFormat) => stringify(
-      node.name,
-      innerFormat(node.children, depth + 1),
-      depth,
-      'blank',
-    ),
-  };
-
   const innerFormat = (innerDiffTree, depth) => {
     const strings = innerDiffTree
       .flatMap((node) => mapping[node.type](node, depth, innerFormat));
